4487440a1ef8d6af7c49fdeb58aee99e29dcb8f0
or0986113303 <or0986113303@gmail.com>
2021-07-29 14:30:56 +0800

    Add basic operator such as create, read, update, delete

---
+++ basicoperatorsqllite/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"os"
+
+	"github.com/sirupsen/logrus"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	gorm.Model
+	Name   string
+	Number uint
+}
+
+var (
+	log = logrus.New()
+)
+
+func init() {
+	log.Out = os.Stdout
+}
+
+func main() {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// Migrate the schema
+	db.AutoMigrate(&Product{})
+
+	// Create
+	db.Create(&Product{Name: "MirChen", Number: 16701})
+
+	// Read
+	var product Product
+	db.First(&product, "Name = ?", "MirChen") // find product with code D42
+
+	log.Info(product.Name)
+	log.Info(product.Number)
+	log.Info(product)
+
+	// Update - update product's price to 200
+	db.Model(&product).Update("Number", 200)
+
+	log.Info(product.Name)
+	log.Info(product.Number)
+	log.Info(product)
+
+	// Update - update multiple fields
+	db.Model(&product).Updates(Product{Number: 201, Name: "Test"}) // non-zero fields
+
+	log.Info(product.Name)
+	log.Info(product.Number)
+	log.Info(product)
+
+	db.Model(&product).Updates(map[string]interface{}{"Number": 202, "Name": "MirChenTest"})
+
+	log.Info(product.Name)
+	log.Info(product.Number)
+	log.Info(product)
+}
