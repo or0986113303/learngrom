9744f43e7f1ad4a57c5a864ed4c20957ceeda2cb
or0986113303 <or0986113303@gmail.com>
2021-07-29 16:26:54 +0800

    basic select first one by key

--- morebasicoperator/main.go
+++ morebasicoperator/main.go
@@ -6,11 +6,14 @@ import (
 	"morebasicoperator/model"
 
 	"github.com/sirupsen/logrus"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 var (
-	user = model.User{Name: "Mir", Number: 16701}
+	user model.User
 	log  = logrus.New()
+	db   *gorm.DB
 )
 
 func init() {
@@ -18,5 +21,24 @@ func init() {
 }
 
 func main() {
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		log.Panic("failed to connect database")
+	}
+
+	// Migrate the schema
+	db.AutoMigrate(&model.User{})
+
+	db.Create(&model.User{})
+
+	log.Info(user)
+
+	if err := db.Where("Number = ?", 16701).Find(&user).Error; err != nil {
+		log.Panic(err)
+	}
+
+	log.Info(user.Name)
+	log.Info(user.Number)
 	log.Info(user)
 }
