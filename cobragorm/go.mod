cc1f7433c5afe56cc6b703637a83f5a33da5e4c8
or0986113303 <or0986113303@gmail.com>
2021-08-03 14:42:54 +0800

    Combine basic insert select by orm method and cobra cmd

--- cobragorm/go.mod
+++ cobragorm/go.mod
@@ -1,14 +1,13 @@
-module morebasicoperator
+module cobragorm
 
 go 1.13
 
 require (
 	github.com/mattn/go-sqlite3 v2.0.3+incompatible // indirect
 	github.com/sirupsen/logrus v1.8.1
-	github.com/spf13/cast v1.4.0 // indirect
-	github.com/spf13/cobra v1.2.1 // indirect
+	github.com/spf13/cobra v1.2.1
+	github.com/spf13/viper v1.8.1
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
-	golang.org/x/text v0.3.6 // indirect
 	gorm.io/driver/sqlite v1.1.4
 	gorm.io/gorm v1.21.12
 )
---
+++ cobragorm/lib/control/user.go
@@ -0,0 +1,122 @@
+package control
+
+import (
+	"cobragorm/lib/model"
+	"log"
+	"reflect"
+	"sync"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type ORMEngine struct {
+	*gorm.DB
+}
+
+var (
+	ormEngine *ORMEngine
+	ormOnce   sync.Once
+)
+
+func NewEngine() {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		log.Panic("failed to connect database")
+	}
+
+	ormOnce.Do(func() {
+		dbh := new(ORMEngine)
+		dbh.DB = db
+		ormEngine = dbh
+	})
+}
+
+func GetEngine() *ORMEngine {
+	return ormEngine
+}
+
+func (USR *ORMEngine) Insert(src model.User) (res model.User) {
+	USR.AutoMigrate(&model.User{})
+	USR.Create(&src)
+	res = src
+	return
+}
+
+func (USR *ORMEngine) SelectConditionLast(Name string, value interface{}) (res model.User) {
+
+	cond := Name + " = ?"
+	r := reflect.ValueOf(&res)
+	switch reflect.Indirect(r).FieldByName(Name).Type().String() {
+	case "uint":
+		if err := USR.Where(cond, value.(int)).Last(&res).Error; err != nil {
+			log.Panic(err)
+		}
+	case "string":
+		if err := USR.Where(cond, value.(string)).Last(&res).Error; err != nil {
+			log.Panic(err)
+		}
+	default:
+		log.Panic("gg")
+	}
+	return
+}
+
+func (USR *ORMEngine) SelectConditionFirst(Name string, value interface{}) (res model.User) {
+
+	cond := Name + " = ?"
+	r := reflect.ValueOf(&res)
+	switch reflect.Indirect(r).FieldByName(Name).Type().String() {
+	case "uint":
+		if err := USR.Where(cond, value.(int)).First(&res).Error; err != nil {
+			log.Panic(err)
+		}
+	case "string":
+		if err := USR.Where(cond, value.(string)).First(&res).Error; err != nil {
+			log.Panic(err)
+		}
+	default:
+		log.Panic("gg")
+	}
+	return
+}
+
+func (USR *ORMEngine) SelectCondition(Name string, value interface{}) (res model.User) {
+
+	cond := Name + " = ?"
+	r := reflect.ValueOf(&res)
+	switch reflect.Indirect(r).FieldByName(Name).Type().String() {
+	case "uint":
+		if err := USR.Where(cond, value.(int)).Find(&res).Error; err != nil {
+			log.Panic(err)
+		}
+	case "string":
+		if err := USR.Where(cond, value.(string)).Find(&res).Error; err != nil {
+			log.Panic(err)
+		}
+	default:
+		log.Panic("gg")
+	}
+	return
+}
+
+func (USR *ORMEngine) OmitName(schema interface{}) (res model.User) {
+
+	switch v := schema.(type) {
+	case string:
+		log.Panic(v)
+	case int32, int64:
+		log.Panic(v)
+	case model.User:
+		op, ok := schema.(model.User)
+		if ok {
+			USR.Omit("Name").Create(&op)
+		} else {
+			log.Panic("convert to other model fail")
+		}
+	default:
+		log.Panic("unknown")
+	}
+
+	return
+}
